eca819a6ede0464ddd60b11d93148271a5ae0d2f
ArnabDas2001 <you@example.com>
2023-10-30 13:32:44 +0530

    landing page layout complete

--- javaScripts/slider.js
+++ javaScripts/slider.js
@@ -1,18 +1,16 @@
-const progressCircle = document.querySelector(".autoplay-progress svg");
-      const progressContent = document.querySelector(".autoplay-progress span");
-      var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true
-        },
-
-        on: {
-          autoplayTimeLeft(s, time, progress) {}
-        }
-      });
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
