264485724539583d563116b71f237c1ba74d4321
Raven <priyadarsi02@gmail.com>
2023-11-03 16:02:42 +0530

    added login php
    need to work

--- html/LoginandRegister/login.php
+++ html/LoginandRegister/login.php
@@ -13,6 +13,56 @@
     <link rel="stylesheet" href="../../style.css">
 
 </head>
+
+<!-- the php part -->
+<?php
+require '../../dbconnect.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = trim($_POST['username']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    if (empty($username) || empty($email) || empty($password)) {
+        echo "<script>alert('Error: Username, email and password all are required.');</script>";
+    } else {
+        if (isset($_POST['role'])) {
+            $role = $_POST['role'];
+
+            if (empty($role)) {
+                echo "<script>alert('Error: Please select a role.');</script>";
+            } else {
+                function loginUser($conn, $role, $username, $email, $password)
+                {
+                    $HASH = password_hash($password, PASSWORD_DEFAULT);
+                    $sql = "SELECT * FROM `$role` WHERE `user_name` = '$username' AND `email` = '$email'";
+                    $result = mysqli_query($conn, $sql);
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        if (password_verify($password, $row['pass'])) {
+                            header("location: ../profiles/$role/$role.html");
+                        } else {
+                            $entered_password = $HASH;
+                            $database_hashed_password = $row['pass'];
+
+                            echo "<script>alert('Error: Incorrect password. Please try again.;</script>";
+                        }
+                    }
+                }
+
+                if ($role == 'teacher' || $role == 'student' || $role == 'company') {
+                    loginUser($conn, $role, $username, $email, $password);
+                } else {
+                    echo "<script>alert('Error: Invalid role selected.');</script>";
+                }
+            }
+        }
+    }
+}
+?>
+
+
+<!-- Here starts the body part -->
 <body>
 
     <div class="login-page">
@@ -22,37 +72,42 @@
 
         <!-- rightside -->
         <div class="right-side">
-
+        <form action="login.php" method="POST">
             <!-- for the re-direct -->
-            <div class="right-card">
-                <button class="btn" id="teacherButton">Teacher</button>
-                <button class="btn" id="studentButton">Student</button>
-                <button class="btn" id="companyButton">Company</button>
+            <div class="select-box">
+                <p class="selecttext">Please select your role:</p>
+                <div class="selectrole">
+                    <select class="roles" name="role" id="roles">
+                        <option class="drpdwn" value="default">Select a role</option>
+                        <option class="drpdwn" value="teacher">Teacher</option>
+                        <option class="drpdwn" value="student">Student</option>
+                        <option class="drpdwn" value="company">Company</option>
+                    </select>
+                </div>
             </div>
 
             <!-- entries -->
-            <div class="entry">
-                <div class="withsubmit">
-                    <div class="inputboxes">
-                        <p class="inphead user-name">User Name</p>
-                        <input class="pleasebox" type="text" placeholder="Enter your username">
+            <div class="withsubmit">
+                <div class="inputboxes">
+                    <p class="inphead user-name">User Name</p>
+                    <input name="username" class="pleasebox" type="text" placeholder="Enter your username">
                         
-                        <p class="inphead email">Email</p>
-                        <input class="pleasebox" type="email" placeholder="Enter the email">
+                    <p class="inphead email">Email</p>
+                    <input name="email" class="pleasebox" type="email" placeholder="Enter the email">
                         
-                        <p class="inphead password">Password</p>
-                        <input class="pleasebox" type="password" placeholder="Password please">
-                    </div>
-                    <div class="subdiv">
-                        <button class="btn submit" id="submitButton" onclick="submitSelection()">Login</button>
-                    </div>
+                    <p class="inphead password">Password</p>
+                    <input name="password" class="pleasebox" type="password" placeholder="Password please">
                 </div>
+                <div class="subdiv">
+                    <button class="btn submit" id="submitButton" onclick="submitSelection()">Login</button>
+                </div>
+            </div>
+        </form>
 
-                <!-- registration re-direct -->
-                <div class="gotoreg">
-                    <p>Don't have an account?</p>
-                    <a href="../LoginandRegister/register.php"><button class="btn">Register Here</button></a>
-                </div>
+            <!-- registration re-direct -->
+            <div class="gotoreg">
+                <p>Don't have an account?</p>
+                <a href="../LoginandRegister/register.php"><button class="btn">Register Here</button></a>
             </div>
         </div>
     </div>
@@ -62,3 +117,26 @@
     <script src="/javaScripts/selectGender.js"></script>
 </body>
 </html>
+
+<!-- require '../../dbconnect.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = ($_POST['username']);
+    $email = ($_POST['email']);
+    $password = ($_POST['password']);
+    $role = ($_POST['role']);
+
+    echo $username, $email, $password, $role;
+
+    $sql = "SELECT * FROM student WHERE `user_name` = '$username' AND `email` = '$email'";
+    $result = mysqli_query($conn, $sql);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        if (password_verify($password, $row['pass'])) {
+            session_start();
+            header("location: ../profiles/student/student.html");
+        } else {
+            echo "<script>alert('Error: Incorrect password. Please try again.);</script>";
+        }
+    }
+} -->
