94317852f882082d7b58529eecdab60d798538ab
Raven <priyadarsi02@gmail.com>
2024-03-04 12:24:08 +0530

    Updated the login style

--- html/MainLanding/mainLanding.php
+++ html/MainLanding/mainLanding.php
@@ -10,23 +10,46 @@
 </head>
 
 <?php
-    header("Content-Type: text/html");
+header("Content-Type: text/html");
 ?>
 
 <body class="mainLandingPage">
-    <div class="navbar">
+    <navbar class="navbar">
         <div class="navbar-brand">Career Connect</div>
         <div class="navbar-buttons">
 
             <!-- the login -->
             <div class="navbar-button" onclick="openLogIn()">Log in</div>
             <div class="logInNav" id="logInNav">
-                <p>The button below will redirect you to the login page for student, teacher and company</p>
-                <a class="linkingLogIn" href="../LoginandRegister/login.php">
-                    <div class="buttonInnerLog" id="buttonInnerLog">Log In -></div>
-                </a>
+                <div class="LoginInterface">
+                    <div class="Tlogin">
+                        <p>
+                            Teacher Login Here
+                        </p>
+                        <a class="linkingLogIn" href="../LoginandRegister/teacherLogin.php">
+                            <div class="buttonInnerLog" id="buttonInnerLog">Teacher Log in -></div>
+                        </a>
+                    </div>
+                    <div class="Slogin">
+                        <p>
+                            Student Login Here
+                        </p>
+                        <a class="linkingLogIn" href="../LoginandRegister/studentLogin.php">
+                            <div class="buttonInnerLog" id="buttonInnerLog">Student Log in -></div>
+                        </a>
+                    </div>
+                    <div class="Clogin">
+                        <p>
+                            Company Login Here
+                        </p>
+                        <a class="linkingLogIn" href="../LoginandRegister/companyLogin.php">
+                            <div class="buttonInnerLog" id="buttonInnerLog">Company Log in -></div>
+                        </a>
+                    </div>
+                
                 <div class="closeLogIn" id="closeLogIn" onclick="closeLogIn()"><i class="fa-solid fa-xmark"></i></div>
             </div>
+        </div>
 
             <!-- the admin page-->
             <div class="navbar-button" onclick="openRegIn()">Register your college</div>
@@ -43,7 +66,11 @@
                 </div>
                 <div class="closeRegIn" id="closeRegIn" onclick="closeRegIn()"><i class="fa-solid fa-xmark"></i></div>
             </div>
+            </div>
         </div>
+    </navbar>
+    <div class="temp">
+        Have some power mate
     </div>
 
     <script src="../../javaScripts/buttonPop.js"></script>
