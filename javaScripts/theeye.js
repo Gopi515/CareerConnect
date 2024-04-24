fb935d2f99eca1ff0b479a145c633a869fc64203
Raven <priyadarsi02@gmail.com>
2024-04-24 17:43:49 +0530

    Made some bug fixes
    > Bug fixes on the eye,
    > Made some new files and folders for upcoming list to make,
    > Changes some core namings for relevance.

--- javaScripts/theeye.js
+++ javaScripts/theeye.js
@@ -1,16 +1,17 @@
 function togglePassword() {
-    const passwordInput = document.getElementById("password");
-    const eyeIcons = document.querySelectorAll(".eye-icon, .eye-iconl");
+  const passwordInput = document.getElementById("password");
+  const eyeIcons = document.querySelectorAll(".eye-icon, .eye-iconl");
 
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-        eyeIcons.forEach(icon => {
-            icon.innerHTML = '<i class="far fa-eye-slash"></i>'; // Change to open eye
-        });
-    } else {
-        passwordInput.type = "password";
-        eyeIcons.forEach(icon => {
-            icon.innerHTML = '<i class="far fa-eye"></i>'; // Change to closed eye
-        });
-    }
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    console.log("hello darkness my old firend");
+    eyeIcons.forEach((icon) => {
+      icon.innerHTML = '<i class="far fa-eye-slash"></i>'; // Change to open eye
+    });
+  } else {
+    passwordInput.type = "password";
+    eyeIcons.forEach((icon) => {
+      icon.innerHTML = '<i class="far fa-eye"></i>'; // Change to closed eye
+    });
+  }
 }
