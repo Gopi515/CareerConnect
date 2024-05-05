c87fb6b7e8799f372e16023fc4c2157d5e514bc3
Arnab Das <dasarnab389@gmail.com>
2024-05-06 02:06:00 +0530

    added skill test/exam

---
+++ html/CEE/adminpanel/admin/pages/home.php
@@ -0,0 +1,13 @@
+<style>
+.manageforTeacher{
+    font-size: 40px;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 70px;
+    margin-left: 300px;
+    font-family: poppins;
+    color: #0083fa;
+}
+</style>
+
+<div class="manageforTeacher">Navigate the side bar to Add or manage courses, question and examinee</div>
---
+++ html/CEE/pages/home.php
@@ -0,0 +1,20 @@
+
+
+<div class="app-main__outer">
+    <div id="refreshData">
+            
+    </div>
+
+<style>
+.manageforStudent{
+    font-size: 40px;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 150px;
+    margin-left: 10px;
+    font-family: poppins;
+    color: #0083fa;
+}
+</style>
+
+<div class="manageforStudent">Navigate the side bar to check out mcq tests based on your skills</div>
