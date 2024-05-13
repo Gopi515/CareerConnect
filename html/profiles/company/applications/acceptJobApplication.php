c6e646eb3da4d3dcb253caa5220ca416420986be
Gopi515 <gopinathghosh514@gmail.com>
2024-05-13 20:35:18 +0530

    shortlisted student job & internship application by company functionality added

---
+++ html/profiles/company/applications/acceptJobApplication.php
@@ -0,0 +1,27 @@
+<?php
+require '../../../../dbconnect.php';
+
+if (isset($_GET['id'])) {
+    $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+    $status = "Company shortlisted your application";
+    $updatedata = "UPDATE `job_applied` SET `status`='$status' WHERE id = $id";
+    $stmt = mysqli_query($conn, $updatedata);
+
+    if ($stmt) {
+        echo '<script type="text/javascript">
+                alert("Reviewed & Forwarded successfully.");
+                window.location.href = "../posted/jobposted.php"; 
+            </script>';
+        exit;
+    } else {
+        echo '<script type="text/javascript">
+                alert("Reviewed & Forwarded unsuccessful.");
+                window.location.href = "../posted/jobposted.php";
+            </script>';
+        exit;
+    }
+
+} else {
+    echo "ID not found in the URL.";
+}
+?>
